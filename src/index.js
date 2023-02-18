43efac6b8a1cd81862b88812a23f9c44a7e51825
anubhav dube <anubhavdube@anubhavs-MacBook-Pro.local>
2023-02-18 23:09:00 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import wallpaper from './assets/nasa.jpg'
+import Rocket from './assets/Rocket.gif'
+// import { useRef } from "react";
+// import { useInView } from 'react-intersection-observer';
+import './index.css'
+export default function App() {
+  // const { ref: magic, inView: magicSection } = useInView();
+  return (
+    <div className="App" style={{ backgroundImage: `url(${wallpaper})`,
+                                  backgroundRepeat : "no-repeat",
+                                  height:1080,width:2056,
+                                  backgroundSize: "contain"}}>  
+                                 
+    <div className="relative left-[200px] top-[100px]" >
+    <span className="animate-waving-hand">
+         <img src={Rocket} alt="error" className="h-[100px]"/>
+    </span>
+    </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
